c0be8482f7047772e5f751e8798ebc2420213326
MZunairKhan <khanzunair15@outlook.com>
2024-07-30 12:11:26 +0500

    Add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import QRScanner from './components/qr-scanner';
+import QRGenerator from './components/qr-generator';
+
+enum Mode {
+  'None',
+  'Generate',
+  'Scan'
+}
 
 function App() {
+
+  const [mode, setMode] = useState<Mode>(Mode.None);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,9 +21,19 @@ function App() {
       </header>
 
       <div className='QR-Section'>
-        <button className='QR-Button'>Generate QR</button>
-        <button className='QR-Button'>Scan QR</button>
+        <button className='QR-Button' onClick={() => setMode(Mode.Generate)}>Generate QR</button>
+        <button className='QR-Button' onClick={() => setMode(Mode.Scan)}>Scan QR</button>
       </div>
+
+      {
+        mode === Mode.Scan && 
+        <QRScanner/>
+      }
+
+      {
+        mode === Mode.Generate && 
+        <QRGenerator/>
+      }
     </div>
   );
 }
---
+++ src/components/qr-generator.tsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+
+function QRGenerator() {
+  return (
+    <div>
+        QR Generator
+    </div>
+  );
+}
+
+export default QRGenerator;
